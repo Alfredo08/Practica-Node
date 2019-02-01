69df3e9b960521dc987ce1cca819204731d031a3
Alfredo Salazar <>
2019-02-01 07:54:54 -0600

    Subiendo archivos a git

---
+++ models/frutasModel.js
@@ -0,0 +1,43 @@
+const mongoose = require('mongoose');
+mongoose.Promise = global.Promise;
+
+const dbFrutas = mongoose.Schema({
+	nombre : {type : String, required : true},
+	id : {type : Number, required : true}
+});
+
+const Frutas = mongoose.model("Frutas", dbFrutas);
+
+const ListaFrutas = {
+	obtener : function(){
+		return inventarioFrutas
+	},
+	crear : function(resolve, reject, nuevaFruta){
+		Frutas.create(nuevaFruta)
+			.then( resultado => {
+				resolve(resultado);
+			})
+			.catch(err => {
+				reject(err);
+			})
+	},
+	actualizar : function (resolve, reject, idFruta, frutaActualizada){
+		Frutas.findByIdAndUpdate(idFruta, {$set : frutaActualizada}, {new : true})
+			.then( resultado => {
+				resolve(resultado);
+			}).
+			catch(err => {
+				reject(err);
+			})
+	}
+}
+
+
+module.exports = {
+	ListaFrutas : ListaFrutas
+};
+
+
+
+
+
